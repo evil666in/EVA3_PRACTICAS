16ed71ec846017bd4970a1cb6092ccd52564e06f
evil666in <158379561+evil666in@users.noreply.github.com>
2024-05-06 18:27:07 -0600

    Add files via upload

---
+++ EVA3_1_EXCEPCIONES/src/eva3_1_excepciones/EVA3_1_EXCEPCIONES.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_excepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_EXCEPCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int num; 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dame un numero que sea entero");
+        num = scan.nextInt();
+        System.out.println("El valor es " + num); 
+        
+    
+    }
+    
+}
